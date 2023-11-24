c22df5728305b295d686e799408d81fa53e42fd4
Александр Вареник <varenik@Company-IT.local>
2023-11-24 09:21:52 +0700

    it is work

---
+++ Proba.cmd
@@ -0,0 +1,13 @@
+rem delete bin and obj for windows command.com only
+rem on linux rem `for` line and run as `bash Proba.cmd`
+rem Warning: install Microsoft.NET.Sdk 6.0
+
+for /D /R  %%i in (bin,obj) do rd /s /q "%%~i"
+
+cd Nemerle.Sdk
+dotnet pack
+cd ../macr
+dotnet build
+cd ../prj
+dotnet run
+
